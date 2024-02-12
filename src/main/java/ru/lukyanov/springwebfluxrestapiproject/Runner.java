5476e10cb176acc4ed7a1108a85fa836fcd5e9e1
Kirill Lukyanov <kirs2000@mail.ru>
2024-02-12 12:13:50 +0300

    Initial commit

---
+++ src/main/java/ru/lukyanov/springwebfluxrestapiproject/Runner.java
@@ -0,0 +1,13 @@
+package ru.lukyanov.springwebfluxrestapiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxRestApiProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebfluxRestApiProjectApplication.class, args);
+    }
+
+}
