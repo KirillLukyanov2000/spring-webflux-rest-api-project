9c52481b1daf57fa4de0d3325ce97c0a0254ba7c
Kirill Lukyanov <101703819+KirillLukyanov2000@users.noreply.github.com>
2024-04-02 17:08:02 +0300

    Update Sender.java

--- src/main/java/ru/lukyanov/kafkaproject/withoutBootConfig/Sender.java
+++ src/main/java/ru/lukyanov/kafkaproject/withoutBootConfig/Sender.java
@@ -1,1 +1,19 @@
 
+public class Sender {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+		context.getBean(Sender.class).send("test", 42);
+	}
+
+	private final KafkaTemplate<Integer, String> template;
+
+	public Sender(KafkaTemplate<Integer, String> template) {
+		this.template = template;
+	}
+
+	public void send(String toSend, int key) {
+		this.template.send("topic1", key, toSend);
+	}
+
+}
